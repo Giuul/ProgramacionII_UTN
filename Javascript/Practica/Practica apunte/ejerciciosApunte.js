570b8b3e164a9197a3b883dfae199606e6465705
Giuli :) <113950045+Giuul@users.noreply.github.com>
2024-10-09 17:41:34 -0300

    Actualizo carpera documentacion de Javascript

---
+++ Javascript/Practica/Practica apunte/ejerciciosApunte.js
@@ -0,0 +1,27 @@
+/*
+-----------EJERCICIO &&------------------------
+let altura = 0;
+let edad = 0;
+altura = parseFloat(prompt("Ingrese al altura"));
+edad = parseInt(prompt("Ingrese la edad"));
+if(altura > 1.30 && edad > 14){
+    console.log("Cumple con los requisitos");
+}else{
+    console.log("No cumple con los requisitos");
+}
+
+-----------EJERCICIO OR------------------------
+let color;
+color = prompt("Ingrese el color del auto"); 
+if(color == "Rojo" || color == "Verde"){
+    console.log("El auto pertenece a la categoria A");
+}else{
+    console.log("El auto pertenece a la categoría B")
+}*/
+
+//----------- EJERCICIO WHILE------------------------
+i = 0;
+while(i < 5){
+    console.log("Valor de i: ", i);
+    i = i + 1;
+}
